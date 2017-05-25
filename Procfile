daf57f0d7efcb2f2a3e98d0ca2531979fd9a9763
Daniel Jones <tortxof@gmail.com>
2017-05-25 18:58:26 -0400

    Remove old unused files

--- Procfile
+++
@@ -1,2 +0,0 @@
-compass: compass watch
-jekyll: jekyll serve
--- config.rb
+++
@@ -1,9 +0,0 @@
-require 'breakpoint'
-require 'susy'
-require 'compass/import-once/activate'
-
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
